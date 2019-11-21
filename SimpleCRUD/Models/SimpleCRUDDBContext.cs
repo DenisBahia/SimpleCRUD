deb824f2bd65b2d851bf0211bddec711b5a88cea
DenisBahia <45142971+DenisBahia@users.noreply.github.com>
2019-11-21 17:15:29 +0100

    1st

---
+++ SimpleCRUD/Models/SimpleCRUDDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCRUD.Models
+{
+    public class SimpleCRUDDBContext: DbContext
+    {
+        public SimpleCRUDDBContext(DbContextOptions<SimpleCRUDDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Books> Books { get; set; }
+    }
+}
